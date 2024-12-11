4997b19bc048a013957750bea0ca1868ed049275
NovaesThiago <novaesthiagoss@gmail.com>
2024-12-11 12:59:03 -0300

    att questão 9

--- q9.js
+++ q9.js
@@ -6,23 +6,23 @@ let imc = Number
 
 imc = (peso/(altura*altura))
 
-switch (true){
-    case (imc<18.5):
-        console.log("Abaixo do peso.")
-        break;
-    case (imc>=18.5 && imc<24.9):
+if (imc<18.5){
+    console.log("Abaixo do peso.")
+    }else if (imc>=18.5 && imc<24.9) {
         console.log("Peso normal.")
-        break;
-    case (imc>=24.9 && imc<29.9):
+        
+    }else if (imc>=24.9 && imc<29.9){
         console.log("Pré-obesidade.")
-        break;
-    case (imc>=29.9 && imc<34.9):
+    
+    }else if (imc>=29.9 && imc<34.9){
         console.log("Obesidade Grau 1.")
-        break;
-    case (imc>=34.9 && imc<39.9):
+    
+    }else if (imc>=34.9 && imc<39.9){
         console.log("Obesidade Grau 2.")
-        break;
-    case (imc>=39.9):
+    
+    }else if (imc>=39.9){
         console.log("Obesidade Grau 3.")
-        break
-}
+   
+    } else {
+        console.log("Resultado inválido.")
+    }
