bc95338f5904d15e31b3fcc55cd2d3bebfd1599f
NovaesThiago <novaesthiagoss@gmail.com>
2024-12-17 00:56:21 -0300

    atividade capitulo 5

--- qextra.js
+++ qextra.js
@@ -1,29 +1,29 @@
 let num = Number(Math.floor(Math.random() * 101))
 let palpite = Number
 let resetGame = Number
-let usuario = Number(100)
+let pontuacao = Number(99)
 
 console.log("////////////////////ADIVINHE O NUMERO!/////////////////////")
 console.log("ADIVINHE UM NÚMERO DE 0 À 100")
-console.log("LEMBRE-SE: O USUÁRIO TEM 10 TENTATIVAS. BOA SORTE!")
-
+console.log("O JOGADOR TERÁ 100 PONTOS, A CADA ERRO ELE PERDERÁ 1 PONTO!")
+console.log("BOA SORTE!")
 
 while (true) {
 
     palpite = prompt("DIGITE O SEU PALPITE: ") 
     if (palpite == num){
         console.log("PARABÉNS! VOCÊ ACERTOU!")
-        console.log("SUA PONTUAÇÃO FOI: ", usuario)
+        console.log("SUA PONTUAÇÃO FOI: ", pontuacao)
         break
     }else if (palpite<0){
         console.log("NÚMERO INVALIDO")
-        usuario = (usuario-1)
+        pontuacao = (pontuacao-1)
     } else if (palpite<num){ 
         console.log("O NÚMERO SECRETO É MAIOR QUE O PALPITE DIGITADO")
-        usuario = (usuario-1)
+        pontuacao = (pontuacao-1)
     }else {
         console.log("O NÚMERO SECRETO É MENOR QUE O PALPITE DIGITADO")
-        usuario = (usuario-1)
+        pontuacao = (pontuacao-1)
     }
 }      
 console.log("PROGAMA ENCERRADO.")
